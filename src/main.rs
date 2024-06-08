0c5db8e99055c674ad3217a933d61c3b821b9046
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-08 18:32:56 -0400

    added default settings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-use macroquad::*;
+use macroquad::prelude::*;
 
-fn main() {
-    println!("Hello, world!");
+fn settings() -> Conf {
+    return Conf {
+        window_title: String::from("Asteroids Clone"),
+        window_width: 800,
+        window_height: 800,
+        high_dpi: true,
+        window_resizable: false,
+        fullscreen: false,
+        ..Default::default()
+    };
+}
+
+#[macroquad::main(settings)]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        next_frame().await;
+    }
 }
