6480a1f79123fc6b02cc4df8edd865e934c20e61
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-09 20:32:55 -0400

    added use statements

--- examples/wasm/main.rs
+++ examples/wasm/main.rs
@@ -1,13 +1,15 @@
+use std::path::Path;
 use axum::Router;
-
+use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main () -> Result<(), Box<dyn std::error::Error>> {
-    if std::path::Path::new("./asteroids.wasm").exists() {
-        panic!("please build the wasm binary and paste it at ./examples/wasm/asteroids.wasm\n");
+    if Path::new("./asteroids.wasm").exists() {
+        panic!("please build the wasm binary and paste it at ./examples/wasm/asteroids.wasm\nsee readme.md");
     }
-    let listener = tokio::net::TcpListener::bind("::1:5000").await?;
-    let files = tower_http::services::ServeDir::new("./examples/wasm/");
+    let listener = TcpListener::bind("::1:5000").await?;
+    let files = ServeDir::new("./examples/wasm/");
     let routes = Router::new().nest_service("/", files) ;
     axum::serve(listener, routes).await?;
     return Ok(());
