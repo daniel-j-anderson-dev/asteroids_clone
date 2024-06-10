a1b18f28c4a3f6782797d3cfb7f88a4c07264799
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-09 21:53:33 -0400

    made step_motion logic more clear

--- src/kinematic.rs
+++ src/kinematic.rs
@@ -7,8 +7,15 @@ pub struct Kinematic {
 }
 impl Kinematic {
     pub fn step_motion(&mut self) {
-        self.velocity += self.acceleration;
-        self.position += self.velocity;
+        let p_0 = self.position.clone();
+        let v_0 = self.velocity.clone();
+        let a_0 = self.acceleration.clone();
+
+        let p_1 = p_0 + v_0;
+        let v_1 = v_0 + a_0;
+
+        self.position = p_1;
+        self.velocity = v_1;
     }
     pub fn step_friction(&mut self) {
         // apply friction (using linear interpolation aka lerp)
