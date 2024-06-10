9d02078b6f4acb0c1d72fc248bd9f18acecdc759
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-09 22:47:57 -0400

    added comment

--- src/asteroid.rs
+++ src/asteroid.rs
@@ -1,2 +1,3 @@
 //! Implement an asteroid struct that is drawn as a spinning hexagon with constant velocity
 //! random position constructor!
+//! look up hexagon on https://docs.rs/macroquad/latest/macroquad/
