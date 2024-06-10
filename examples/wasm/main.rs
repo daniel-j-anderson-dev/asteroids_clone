7405f031af89758dc102d027e3ad2881d5752b16
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-09 21:21:16 -0400

    added print statement for convince

--- examples/wasm/main.rs
+++ examples/wasm/main.rs
@@ -1,16 +1,17 @@
-use std::path::Path;
 use axum::Router;
+use std::path::Path;
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 #[tokio::main]
-async fn main () -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if Path::new("./asteroids.wasm").exists() {
         panic!("please build the wasm binary and paste it at ./examples/wasm/asteroids.wasm\nsee readme.md");
     }
     let listener = TcpListener::bind("::1:5000").await?;
     let files = ServeDir::new("./examples/wasm/");
-    let routes = Router::new().nest_service("/", files) ;
+    let routes = Router::new().nest_service("/", files);
+    println!("http://localhost:5000");
     axum::serve(listener, routes).await?;
     return Ok(());
 }
