454be33b7f4f876d0d065ff7dd1068982cb584d1
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-09 23:07:46 -0400

    added comments

--- src/asteroid.rs
+++ src/asteroid.rs
@@ -1,2 +1,6 @@
 //! Implement an asteroid struct that is drawn as a spinning hexagon with constant velocity
 //! random position constructor!
+//! - figure out where vertexes are relative to the origin
+//! - rotate those vertexes according to the rotation matrix
+//! - add those rotated vertexes to the position of the hexagon
+//! - draw a line between each vertex
