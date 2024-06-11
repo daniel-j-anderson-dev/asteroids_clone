4cee76fb44a6016b7ab28c81620ceeed24650378
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-11 11:41:27 -0400

    Added a trait so any f32 can have its rotation matrix calculated

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,22 @@ pub mod kinematic;
 pub mod player;
 
 pub use crate::{asteroid::*, bullet::*, kinematic::*, player::*};
+use core::borrow::Borrow;
 use macroquad::prelude::*;
+
+pub trait RotationMatrix {
+    fn angle(&self) -> f32;
+    fn rotation_matrix(&self) -> Mat2 {
+        let sin = self.angle().sin();
+        let cos = self.angle().cos();
+        return mat2(vec2(cos, sin), vec2(-sin, cos));
+    }
+}
+impl<T: Borrow<f32>> RotationMatrix for T {
+    fn angle(&self) -> f32 {
+        return *self.borrow();
+    }
+}
 
 pub fn screen_dimensions() -> Vec2 {
     return vec2(screen_width(), screen_height());
