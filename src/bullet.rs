5b7a95de004671f3b9d3c0f1660a36f4af20ba22
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-11 11:12:10 -0400

    added comments for bullet

--- src/bullet.rs
+++ src/bullet.rs
@@ -1,4 +1,6 @@
 //! make a bullet struct that
+//! - is a small circle
 //! - has a constant velocity
 //! - Starts from the front of the player
+//! - travels in the direction of the player
 //! - disappears after n frames
