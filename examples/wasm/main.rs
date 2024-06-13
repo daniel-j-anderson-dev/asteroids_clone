47c130e7d33787a288a267c8eb67afb5cc0ce453
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-13 17:43:54 -0400

    build wasm every time

--- examples/wasm/main.rs
+++ examples/wasm/main.rs
@@ -8,34 +8,26 @@ use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    // is the wasm binary already in place?
-    if !Path::new("./asteroids.wasm").exists() {
-        // is the wasm binary already at the default build location? (path relative to this file)
-        if !Path::new("./../../target/wasm32-unknown-unknown/release/asteroids.wasm").exists() {
-            // build wasm in default build location if it's not there
 
-            println!("Checking for wasm32-unknown-unknown target; please wait...");
-            let output = Command::new("rustup")
-                .args(&["target", "add", "wasm32-unknown-unknown"])
-                .output()
-                .expect("Failed to add wasm target. is rustup installed?");
-            print_output("wasm target is available", output);
+    println!("Checking for wasm32-unknown-unknown target; please wait...");
+    let output = Command::new("rustup")
+        .args(&["target", "add", "wasm32-unknown-unknown"])
+        .output()
+        .expect("Failed to add wasm target. is rustup installed?");
+    print_output("wasm target is available", output);
 
-            println!("Building wasm; please wait...");
-            let output = Command::new("cargo")
-                .args(&["build", "--release", "--target", "wasm32-unknown-unknown"])
-                .output()
-                .expect("Failed to build wasm binary.");
-            print_output("wasm binary built!", output);
-        }
+    println!("Building wasm; please wait...");
+    let output = Command::new("cargo")
+        .args(&["build", "--release", "--target", "wasm32-unknown-unknown"])
+        .output()
+        .expect("Failed to build wasm binary.");
+    print_output("wasm binary built!", output);
 
-        // copy the wasm binary from the default build location to this example directory (path relative to Cargo.toml)
-        std::fs::copy(
-            "./target/wasm32-unknown-unknown/release/asteroids.wasm",
-            "./examples/wasm/asteroids.wasm",
-        )
-        .expect("Failed to copy wasm binary to example folder");
-    }
+    std::fs::copy(
+        "./target/wasm32-unknown-unknown/release/asteroids.wasm",
+        "./examples/wasm/asteroids.wasm",
+    )
+    .expect("Failed to copy wasm binary to example folder");
 
     println!("wasm binary is in built and in place\nstarting server...");
 
