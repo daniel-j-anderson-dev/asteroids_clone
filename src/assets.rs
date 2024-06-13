348c554ecbd43ec36b35d03e8b43d7fc1284f010
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-13 17:07:06 -0400

    updated assets

--- src/assets.rs
+++ src/assets.rs
@@ -1,11 +1,11 @@
 use macroquad::prelude::*;
 
-pub const DUCK_BMP: &'static [u8] = include_bytes!("../assets/duck.bmp");
+pub const DUCK_BMP: &'static [u8] = include_bytes!("../assets/duck.png");
 pub fn duck_texture() -> Texture2D {
     Texture2D::from_file_with_format(DUCK_BMP, None)
 }
 
-pub const ROCK_BMP: &'static [u8] = include_bytes!("../assets/rock.bmp");
+pub const ROCK_BMP: &'static [u8] = include_bytes!("../assets/rock.png");
 pub fn rock_texture() -> Texture2D {
     Texture2D::from_file_with_format(ROCK_BMP, None)
 }
