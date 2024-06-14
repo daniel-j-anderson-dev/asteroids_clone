d6e356d52ab792374cef73521e14837db35afa2a
Andrew Phoenix <mrtopnacho@gmail.com>
2024-06-13 21:52:20 -0400

    Implementation for bullet

--- src/bullet.rs
+++ src/bullet.rs
@@ -1,37 +1,45 @@
 //! make a bullet struct that
 //! - is a small circle
 //! - has a constant velocity
-//! - Starts from the front of the player
-//! - travels in the direction of the player
+//! - Starts from the front of the player (a vertex)
+//! - travels in the direction of the player (an angle)
 //! - disappears after n frames
 
-use crate::Kinematic;
+use crate::{polar_vec2, Kinematic};
 use crate::{Draw, KinematicGetters, Player};
 use macroquad::prelude::*;
 pub struct Bullet {
     kinematic: Kinematic,
-    size: f32,
 }
 impl Bullet {
-    // fn default() -> Self {
-    //     let player = Player::default();
-    //     return Bullet {
-    //         kinematic: Kinematic {
-    //             position: vec2(0.0, 0.0),
-    //             velocity: vec2(0.0, 0.0),
-    //             acceleration: vec2(0.0, 0.0),
-    //         },
-    //         size: 5.0,
-    //     }
-    // }
+    const SIZE: f32 = Player::SIZE / 10.0;
+    const SPEED: f32 = Player::MAX_SPEED / 4.0;
+
+    pub fn new(player_front: Vec2, angle: f32) -> Self {
+        let velocity = polar_vec2(Self::SPEED, angle);
+        
+        return Bullet {
+            kinematic: Kinematic::new(player_front, velocity, Vec2::ZERO),
+        }; 
+    }
+    pub fn step(&mut self) {
+        self.kinematic.step_motion();
+        self.kinematic.keep_on_screen();
+    }
 }
 impl KinematicGetters for Bullet {
     fn kinematic(&self) -> &Kinematic {
         return &self.kinematic;
     }
 }
-// impl Draw for Bullet {
-//     fn draw(&self) {
-//         draw_circle(x, y, 1.5, WHITE)
-//     }
-// }
+impl Draw for Bullet {
+    fn draw(&self) {
+        let position = self.position();
+        draw_circle(position.x, position.y, Self::SIZE, WHITE);
+    }
+}
+impl From<&Player> for Bullet {
+    fn from(player: &Player) -> Self {
+        return Bullet::new(player.vertices()[0], player.orientation());
+    }
+}
