514db9d263f9cdd70eb4f198fe1701959a633c30
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-06-18 22:06:16 -0400

    removed unused function

--- src/lib.rs
+++ src/lib.rs
@@ -9,33 +9,6 @@ use macroquad::prelude::*;
 
 const FRAC_SQRT3_2: f32 = 0.86602540378443864676372317075294;
 
-/// This function checks every asteroid-bullet combination and asteroid-player combinations for collisions.
-/// ## Returns
-/// - A list of asteroid-bullet collision indexes
-/// - A bool that represents if there was a asteroid-player collision
-pub fn calculate_collisions(asteroids: &[Asteroid], bullets: &[Bullet], player: &Player) -> (Vec<(usize, usize)>, bool) {
-    let mut collision_indexes = Vec::new();
-    let mut player_collision = false;
-
-    for i in 0..asteroids.len() {
-        for j in 0..bullets.len() {
-            // if the bullet is inside the asteroid
-            if is_point_in_hexagon(
-                bullets[j].position(),
-                asteroids[i].position(),
-                asteroids[i].orientation(),
-                asteroids[i].size(),
-            ) {
-                // store the indexes to be processed later
-                collision_indexes.push((i, j));
-            }
-        }
-        // TODO: check for collision with player and asteroid and print on all collisions
-    }
-
-    return (collision_indexes, player_collision);
-}
-
 pub fn is_point_in_hexagon(
     point: Vec2,
     hexagon_center: Vec2,
